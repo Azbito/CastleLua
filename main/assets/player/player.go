25e8cf1cb802089eaeeb8b1a40a6228fbef125ae
Thiago <101950809+Azbito@users.noreply.github.com>
2023-05-01 23:34:24 -0300

    CHORE: Debug function & attack animation

--- main/assets/player/player.go
+++ main/assets/player/player.go
@@ -44,6 +44,7 @@ embedded_components {
   "group: \"player\"\n"
   "mask: \"ground\"\n"
   "mask: \"wall\"\n"
+  "mask: \"danger\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
